63ef084b6aa6f757bb0c2db21f120e772346eb5c
Kishan Mandora <kishanmandora7@gmail.com>
2020-12-19 15:56:04 +0530

    refactor: made code more readable

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const synth = window.speechSynthesis;
-const voices = synth.getVoices();
-let url = "";
-const submitBtn = document.querySelector(".submit-btn");
-const msgTxt = document.querySelector(".msg-txt");
 const yodaBtn = document.querySelector(".yoda");
 const sithBtn = document.querySelector(".sith");
 const mandoBtn = document.querySelector(".mando");
-const mainImg = document.querySelector(".main-img");
-const myVoice = voices[45];
+const msgTxt = document.querySelector(".msg-txt");
 const inputTxt = document.querySelector(".input-txt");
 const outputTxt = document.querySelector(".output-txt");
-const cngBtns = document.querySelectorAll("ul button");
+const mainImg = document.querySelector(".main-img");
+const submitBtn = document.querySelector(".submit-btn");
+const synth = window.speechSynthesis;
+const voices = synth.getVoices();
+const myVoice = voices[45];
+let url = "";
 
 function defaultOp() {
-  mainImg.setAttribute("src", "./images/yoda.gif");
-  msgTxt.innerText = "May the force be with you";
-  url = "https://api.funtranslations.com/translate/yoda.json";
-  yodaBtn.style.backgroundColor = "#95ff64";
-  mandoBtn.style.backgroundColor = "#ffd7f6";
+  submitBtn.addEventListener("click", speakMe);
+  mainImg.setAttribute("src", "./images/mando.gif");
+  msgTxt.innerText = "There is still hope";
+  url = "https://api.funtranslations.com/translate/mandalorian.json";
+  mandoBtn.style.backgroundColor = "#ff853e";
   sithBtn.style.backgroundColor = "#ffd7f6";
+  yodaBtn.style.backgroundColor = "#ffd7f6";
 }
 
 defaultOp();
@@ -30,7 +30,7 @@ function speakMe() {
     .then((json) => json.contents.translated)
     .then((txt) => {
       outputTxt.innerText = txt;
-      var utterThis = new SpeechSynthesisUtterance(txt);
+      let utterThis = new SpeechSynthesisUtterance(txt);
       utterThis.voice = myVoice;
       utterThis.pitch = 0.5;
       utterThis.rate = 0.85;
@@ -38,7 +38,14 @@ function speakMe() {
     });
 }
 
-yodaBtn.addEventListener("click", defaultOp);
+yodaBtn.addEventListener("click", () => {
+  mainImg.setAttribute("src", "./images/yoda.gif");
+  msgTxt.innerText = "May the force be with you";
+  url = "https://api.funtranslations.com/translate/yoda.json";
+  yodaBtn.style.backgroundColor = "#95ff64";
+  mandoBtn.style.backgroundColor = "#ffd7f6";
+  sithBtn.style.backgroundColor = "#ffd7f6";
+});
 
 sithBtn.addEventListener("click", () => {
   mainImg.setAttribute("src", "./images/sith.gif");
@@ -49,12 +56,4 @@ sithBtn.addEventListener("click", () => {
   yodaBtn.style.backgroundColor = "#ffd7f6";
 });
 
-mandoBtn.addEventListener("click", () => {
-  mainImg.setAttribute("src", "./images/mando.gif");
-  msgTxt.innerText = "There is still hope";
-  url = "https://api.funtranslations.com/translate/mandalorian.json";
-  mandoBtn.style.backgroundColor = "#ff853e";
-  sithBtn.style.backgroundColor = "#ffd7f6";
-  yodaBtn.style.backgroundColor = "#ffd7f6";
-});
-submitBtn.addEventListener("click", speakMe);
+mandoBtn.addEventListener("click", defaultOp);
